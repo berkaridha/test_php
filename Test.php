0618a571f6771c285717630482ac176a32834c49
berka <berkaridha@gmail.com>
2023-03-20 12:12:30 +0700

    Berka

---
+++ Test.php
@@ -0,0 +1,12 @@
+<?php
+    $a="12";
+    $b=28;
+
+    $hasil=$a+$b;
+
+    $cetak = print "hasil = $hasil <br>";
+
+    echo("$cetak <br>");
+    echo "Hello world! <br>";
+    printf("Hello world 2! <br>");
+?>
